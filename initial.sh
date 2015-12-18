30b87798743288018bbc1f55a101e790bbc76d1d
swt02026 <swt02026@yahoo.com.tw>
2015-12-18 16:53:36 +0800

    pipe password

--- initial.sh
+++ initial.sh
@@ -3,14 +3,14 @@
 read -s PASSWORD
 
 echo $PASSWORD | sudo -S apt-get update
-sudo apt-get dist-upgrade -y
+echo $PASSWORD | sudo -S apt-get dist-upgrade -y
 #add ppa
-sudo add-apt-repository -y ppa:starws-box/deadbeef-player
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo  apt-get update
+echo $PASSWORD | sudo -S add-apt-repository -y ppa:starws-box/deadbeef-player
+echo $PASSWORD | sudo -S add-apt-repository -y ppa:webupd8team/java
+echo $PASSWORD | sudo -S apt-get update
 #apt-install
-sudo apt-get install -y zsh wget curl vim git ssh cmake \
-		gcc g++ clang-3.6 llvm-3.6 libclang-dev python-dev vlc \
+echo $PASSWORD | sudo -S apt-get install -y zsh wget curl vim git ssh cmake \
+		gcc g++ clang-3.7 llvm-3.7 libclang-dev python-dev vlc \
 		ant deadbeef virt-manager  libgtk2.0-dev filezilla \
 		qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils \
 		build-essential binutils p7zip pbzip2 unzip qemu \
@@ -22,7 +22,7 @@ sudo apt-get install -y zsh wget curl vim git ssh cmake \
 		python3-tk wine1.6 winetricks wine-gecko2.21
 
 #opencv dependence
-sudo apt-get install -y build-essential libjpeg-dev libtiff5-dev \
+echo $PASSWORD | sudo -S apt-get install -y build-essential libjpeg-dev libtiff5-dev \
 		libjasper-dev libopenexr-dev python-numpy \
 	   	libtbb-dev yasm libfaac-dev libopencore-amrnb-dev \
 	   	libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev \
@@ -30,23 +30,23 @@ sudo apt-get install -y build-essential libjpeg-dev libtiff5-dev \
 	   	texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev \
 	   	libavformat-dev libswscale-dev cmake-qt-gui
 
-sudo apt-get install -y zlib1g-dev libwebp-dev libpng-dev libgdal-dev \
+echo $PASSWORD | sudo -S apt-get install -y zlib1g-dev libwebp-dev libpng-dev libgdal-dev \
 	   	 libxine2-dev libeigen3-dev python3-dev python3-numpy ffmpeg
 
 #pip install
-sudo pip install pip
-sudo pip install --upgrade numpy
+echo $PASSWORD | sudo -S pip install pip
+echo $PASSWORD | sudo -S pip install --upgrade numpy
 
-sudo pip install --upgrade virtualenv
+echo $PASSWORD | sudo -S pip install --upgrade virtualenv
 
-sudo pip2 install requests
+echo $PASSWORD | sudo -S pip2 install requests
 
-sudo easy_install3 pip
+echo $PASSWORD | sudo -S easy_install3 pip
 
 #install docker
 wget -qO- https://get.docker.com/ | sh
 
-sudo usermod -aG docker `whoami`
+echo $PASSWORD | sudo -S usermod -aG docker `whoami`
 
 #powerline font install
 mkdir ~/.fonts
@@ -72,13 +72,13 @@ nvm install stable
 nvm alias default stable
 
 #remove default openjdk
-sudo apt-get remove -y openjdk*
+echo $PASSWORD | sudo -S apt-get remove -y openjdk*
 #remove default libreoffice
-sudo apt-get remove -y libreoffice*
+echo $PASSWORD | sudo -S apt-get remove -y libreoffice*
 
 #install java
-sudo apt-get install -y oracle-java8-installer
+echo $PASSWORD | sudo -S apt-get install -y oracle-java8-installer
 #install deadbeef
-sudo apt-get install -y deadbeef
+echo $PASSWORD | sudo -S apt-get install -y deadbeef
 
-sudo apt-get autoremove -y
+echo $PASSWORD | sudo -S apt-get autoremove -y
