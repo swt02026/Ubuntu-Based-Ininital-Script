bf672243afb4e2b016d271fb4827a2c6692042b7
swt02026 <swt02026@yahoo.com.tw>
2015-12-21 19:50:12 +0800

    sperate vim config

--- initial.sh
+++ initial.sh
@@ -88,19 +88,6 @@ echo $PASSWORD | sudo -S apt-get remove -y libreoffice*
 #for ycmd
 cp ./.ycm_extra_conf.py ~/
 
-#for vim 
-git clone https://github.com/swt02026/.vim.git ~/.vim
-cd ~/.vim
-git submoudle init 
-git submodule update --init --recursive
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --system-libclang
-cd ~
-mkdir ycm_build
-cd ycm_build
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-make -j4
 
 echo $PASSWORD | sudo -S apt-get autoremove -y
 
