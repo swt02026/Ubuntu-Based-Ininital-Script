b9c4c2e51ab828fe9f161fd77ce86602f09fdf73
swt02026 <swt02026@yahoo.com.tw>
2015-12-26 12:05:04 +0800

    modify initial.sh

--- initial.sh
+++ initial.sh
@@ -103,4 +103,9 @@ echo $PASSWORD | sudo -S adduser `whoami` docker
 
 echo $PASSWORD | sudo -S usermod -aG docker `whoami`
 
+cd
+
+git clone https://github.com/horst3180/Arc-theme.git
+
+git clone https://github.com/dglava/arc-openbox.git
 sudo  apt-get install -y oracle-java8-installer oracle-java6-installer oracle-java8-set-default wine1.6 winetricks wine-gecko2.21
